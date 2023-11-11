b5bb0eca21d9d7ba50efda5a2b1a62888942590b
Leandro Klyte <klyte45@hotmail.com>
2023-11-11 20:07:04 -0300

    Upgrading to new mod settings setup

--- Utils/BasicIMod.cs
+++ Utils/BasicIMod.cs
@@ -4,11 +4,7 @@ using Colossal.IO.AssetDatabase;
 using Colossal.Localization;
 using Colossal.UI;
 using Game;
-using Game.Reflection;
 using Game.SceneFlow;
-using Game.UI.Localization;
-using Game.UI.Menu;
-using Game.UI.Widgets;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -27,9 +23,6 @@ namespace Belzont.Interfaces
             Redirector.OnWorldCreated(m_updateSystem.World);
             LoadLocales();
 
-            var optionsList = ((List<OptionsUISystem.Page>)typeof(OptionsUISystem).GetProperty("options", RedirectorUtils.allFlags).GetValue(updateSystem.World.GetOrCreateSystemManaged<OptionsUISystem>()));
-            optionsList.Add(Instance.BuildModPage());
-
             var uiSys = GameManager.instance.userInterface.view.uiSystem;
             LogUtils.DoLog($"CouiHost => {CouiHost}");
             ((DefaultResourceHandler)uiSys.resourceHandler).HostLocationsMap.Add(CouiHost, new List<string> { ModInstallFolder });
@@ -38,11 +31,14 @@ namespace Belzont.Interfaces
 
         }
         public abstract void OnDispose();
+        public abstract BasicModData CreateSettingsFile();
         public void OnLoad()
         {
-            Instance = this;
+            ModData = CreateSettingsFile();
+            ModData.RegisterInOptionsUI();
+            AssetDatabase.global.LoadSettings(SafeName, ModData);
+
             KFileUtils.EnsureFolderCreation(ModSettingsRootFolder);
-            LoadModData();
             Redirector.PatchAll();
 
             Type[] newComponents = ReflectionUtils.GetStructForInterfaceImplementations(typeof(IComponentData), new[] { GetType().Assembly })
@@ -51,7 +47,7 @@ namespace Belzont.Interfaces
             if (newComponents.Length > 0)
             {
                 LogUtils.DoInfoLog($"Registering {newComponents.Length} components found at mod {SimpleName}");
-                if (BasicIMod.DebugMode)
+                if (DebugMode)
                 {
                     LogUtils.DoLog("Loaded component count: " + TypeManager.GetTypeCount());
                     LogUtils.DoLog("Loading found components:\n\t" + string.Join("\n\t", newComponents.Select(x => x.ToString())));
@@ -75,21 +71,8 @@ namespace Belzont.Interfaces
         public abstract void DoOnCreateWorld(UpdateSystem updateSystem);
 
         public abstract void DoOnLoad();
-        public string ModDataFilePath => Path.Combine(ModSettingsRootFolder, "settings.xml");
-        public abstract void SaveModData();
-        public abstract void LoadModData();
-        public abstract IBasicModData BasicModData { get; }
 
         #region Saved shared config
-        public static bool DebugMode
-        {
-            get => Instance.BasicModData?.DebugMode ?? false;
-            set
-            {
-                Instance.BasicModData.DebugMode = value;
-                Instance.SaveModData();
-            }
-        }
         public static string CurrentSaveVersion { get; }
         #endregion
 
@@ -107,7 +90,10 @@ namespace Belzont.Interfaces
         #endregion
 
         #region Old CommonProperties Static
-        public static BasicIMod Instance { get; private set; }
+        public static BasicIMod Instance => ModData.ModInstance;
+        public static BasicModData ModData { get; private set; }
+        public static bool DebugMode => ModData.DebugMode;
+
 
         private static ulong m_modId;
         public static ulong ModId
@@ -182,29 +168,6 @@ namespace Belzont.Interfaces
         #endregion
 
         #region UI
-        internal OptionsUISystem.Page BuildModPage()
-        {
-            OptionsUISystem.Page page = new OptionsUISystem.Page
-            {
-                id = $"K45_{Instance.Acronym}"
-            };
-            List<OptionsUISystem.Section> sections = page.sections;
-            sections.AddRange(GenerateModOptionsSections());
-            OptionsUISystem.Section section = new()
-            {
-                id = $"K45.{Instance.Acronym}.About",
-                items = new List<IWidget>
-                {
-                    AddBoolField($"K45.{Acronym}.DebugMode", new DelegateAccessor<bool>(()=>DebugMode,(x)=>DebugMode=x)),
-                    AddValueField($"K45.{Instance.Acronym}.Version",()=>FullVersion)
-                }
-            };
-            sections.Add(section);
-            return page;
-        }
-
-        protected abstract IEnumerable<OptionsUISystem.Section> GenerateModOptionsSections();
-
         private void LoadLocales()
         {
             var file = Path.Combine(ModInstallFolder, $"i18n/i18n.csv");
@@ -225,7 +188,7 @@ namespace Belzont.Interfaces
                             GameManager.instance.localizationManager.AddSource(lang, i18nFile);
                         }
                     }
-                    GameManager.instance.localizationManager.AddSource(lang, new ModGenI18n());
+                    GameManager.instance.localizationManager.AddSource(lang, new ModGenI18n(ModData));
                 }
 
             }
@@ -233,71 +196,27 @@ namespace Belzont.Interfaces
 
         private static Dictionary<string, string> LocaleFileForColumn(IEnumerable<string[]> fileLines, int valueColumn)
         {
-            return fileLines.Skip(1).Where(x => x.ElementAtOrDefault(valueColumn) != default).GroupBy(x => x[0]).ToDictionary(x => x.Key, x => RemoveQuotes(x.First().ElementAtOrDefault(valueColumn)));
+            return fileLines.Skip(1).Where(x => x.ElementAtOrDefault(valueColumn) != default).GroupBy(x => x[0]).ToDictionary(x => ProcessKey(x.Key, ModData), x => RemoveQuotes(x.First().ElementAtOrDefault(valueColumn)));
+        }
+
+        private static string ProcessKey(string key, BasicModData modData)
+        {
+            if (!key.StartsWith("::")) return key;
+            var prefix = key[..3];
+            var suffix = key[3..];
+            return prefix switch
+            {
+                "::L" => modData.GetOptionLabelLocaleID(suffix),
+                "::G" => modData.GetOptionGroupLocaleID(suffix),
+                "::D" => modData.GetOptionDescLocaleID(suffix),
+                "::T" => modData.GetOptionTabLocaleID(suffix),
+                "::W" => modData.GetOptionWarningLocaleID(suffix),
+                "::E" => suffix.Split(".", 2) is string[] enumVal && enumVal.Length == 2 ? modData.GetEnumValueLocaleID(enumVal[0], enumVal[1]) : suffix,
+                _ => suffix
+            };
         }
 
         private static string RemoveQuotes(string v) => v != null && v.StartsWith("\"") && v.EndsWith("\"") ? v[1..^1] : v;
-
-        private static bool GetButtonsGroup(string groupName, out ButtonRow buttons, Button item)
-        {
-            var AutomaticSettingsSButtonGroups = typeof(AutomaticSettings).GetField("sButtonGroups", RedirectorUtils.allFlags);
-            var sButtonGroups = (Dictionary<string, ButtonRow>)AutomaticSettingsSButtonGroups.GetValue(null);
-            if (sButtonGroups.TryGetValue(groupName, out buttons))
-            {
-                Button[] array = new Button[buttons.children.Length + 1];
-                buttons.children.CopyTo(array, 0);
-                array[array.Length - 1] = item;
-                buttons.children = array;
-                return false;
-            }
-            buttons = new ButtonRow
-            {
-                children = new Button[]
-                {
-                    item
-                }
-            };
-            sButtonGroups.Add(groupName, buttons);
-            return true;
-        }
-
-        protected IWidget AddOptionField<T>(string path, string groupName, Action<T> onSet, T value, Func<bool> disabledFn = null, string warningI18n = null)
-        {
-            return GetButtonsGroup(groupName, out ButtonRow item, new ButtonWithConfirmation
-            {
-                path = path,
-                displayName = $"Options.OPTION[{path}]",
-                action = delegate
-                {
-                    onSet(value);
-                },
-                disabled = disabledFn,
-                confirmationMessage = (warningI18n != null ? new LocalizedString?(LocalizedString.Id(warningI18n)) : null)
-            })
-                ? item
-                : (IWidget)null;
-        }
-
-        protected IWidget AddBoolField(string path, DelegateAccessor<bool> accessor, Func<bool> disabledFn = null)
-        {
-            return new ToggleField
-            {
-                path = path,
-                displayName = $"Options.OPTION[{path}]",
-                accessor = accessor,
-                disabled = disabledFn
-            };
-        }
-
-        protected IWidget AddValueField(string path, Func<string> getter)
-        {
-            return new ValueField
-            {
-                path = path,
-                displayName = $"Options.OPTION[{path}]",
-                accessor = new DelegateAccessor<string>(getter, (x) => { })
-            };
-        }
 
         #endregion
 
@@ -342,17 +261,22 @@ namespace Belzont.Interfaces
 
         private class ModGenI18n : IDictionarySource
         {
+            private BasicModData modData;
+            public ModGenI18n(BasicModData data)
+            {
+                modData = data;
+            }
 
             public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
             {
                 return new Dictionary<string, string>
                 {
-                    [$"Options.SECTION[K45_{Instance.Acronym}]"] = Instance.GeneralName,
-                    [$"Options.TAB[K45.{Instance.Acronym}.About]"] = "About",
-                    [$"Options.OPTION[K45.{Instance.Acronym}.DebugMode]"] = "Debug Mode",
-                    [$"Options.OPTION_DESCRIPTION[K45.{Instance.Acronym}.DebugMode]"] = "Turns on the log debugging for this mod",
-                    [$"Options.OPTION[K45.{Instance.Acronym}.Version]"] = "Mod Version",
-                    [$"Options.OPTION_DESCRIPTION[K45.{Instance.Acronym}.Version]"] = "The current mod version.\n\nThe 4th digit being higher than 1000 indicates beta version.",
+                    [modData.GetSettingsLocaleID()] = Instance.GeneralName,
+                    [modData.GetOptionTabLocaleID(BasicModData.kAboutTab)] = "About",
+                    [modData.GetOptionLabelLocaleID(nameof(BasicModData.DebugMode)).Replace(modData.GetType().Name, nameof(BasicModData))] = "Debug Mode",
+                    [modData.GetOptionDescLocaleID(nameof(BasicModData.DebugMode)).Replace(modData.GetType().Name, nameof(BasicModData))] = "Turns on the log debugging for this mod",
+                    [modData.GetOptionLabelLocaleID(nameof(BasicModData.Version)).Replace(modData.GetType().Name, nameof(BasicModData))] = "Mod Version",
+                    [modData.GetOptionDescLocaleID(nameof(BasicModData.Version)).Replace(modData.GetType().Name, nameof(BasicModData))] = "The current mod version.\n\nThe 4th digit being higher than 1000 indicates beta version.",
                 };
             }
 
@@ -362,36 +286,4 @@ namespace Belzont.Interfaces
         }
     }
 
-    public abstract class BasicIMod<D> : BasicIMod where D : class, IBasicModData
-    {
-        public abstract D CreateNewModData();
-
-        public D ModData { get; private set; }
-
-        public sealed override IBasicModData BasicModData => ModData;
-
-        public sealed override void SaveModData()
-        {
-            File.WriteAllText(ModDataFilePath, XmlUtils.DefaultXmlSerialize(ModData));
-        }
-        public sealed override void LoadModData()
-        {
-            if (File.Exists(ModDataFilePath))
-            {
-                try
-                {
-                    ModData = XmlUtils.DefaultXmlDeserialize<D>(File.ReadAllText(ModDataFilePath)) ?? CreateNewModData();
-                }
-                catch
-                {
-                    LogUtils.DoWarnLog("The settings.json was invalid! Generating new data.");
-                    ModData = CreateNewModData();
-                }
-            }
-            else
-            {
-                ModData = CreateNewModData();
-            }
-        }
-    }
 }
---
+++ Utils/BasicModData.cs
@@ -0,0 +1,35 @@
+﻿using Game.Modding;
+using Game.Settings;
+
+namespace Belzont.Interfaces
+{
+    public abstract class BasicModData : ModSetting
+    {
+        public const string kAboutTab = "About";
+
+        public BasicIMod ModInstance { get; private set; }
+        public BasicModData Instance { get; private set; }
+
+        protected BasicModData(IMod mod) : base(mod)
+        {
+            ModInstance = mod as BasicIMod;
+        }
+
+        [SettingsUISection(kAboutTab, null)]
+        public bool DebugMode { get; set; }
+
+        [SettingsUISection(kAboutTab, null)]
+        public string Version => BasicIMod.FullVersion;
+
+        public sealed override void SetDefaults()
+        {
+            DebugMode = false;
+            OnSetDefaults();
+        }
+
+        public abstract void OnSetDefaults();
+
+        public string GetEnumValueLocaleID(string classname, string value) => $"Options.{id}.{classname.ToUpper()}[{value}]";
+
+    }
+}
