f99b99732405224a8d96c2358ca3afaeecff8b56
Leandro Klyte <klyte45@hotmail.com>
2024-03-03 20:09:15 -0300

    Adding extension utility for float vectors

---
+++ Utils/Float3Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Kwytto.Utils
+{
+    public static class Float3Extensions
+    {
+        public static float GetAngleXZ(this float3 dir) => Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+        public static float SqrDistance(this float3 a, float3 b)
+        {
+            float3 vector = new float3(a.x - b.x, a.y - b.y, a.z - b.z);
+            return (vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z);
+        }
+
+        //public static segment3 ToRayY(this float3 vector) => new Segment3(new float3(vector.x, -999999f, vector.z), new float3(vector.x, 999999f, vector.z));
+    }
+}
