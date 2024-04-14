55e3d50f1b05e9007992f8538bb0669838d4ca44
Leandro Klyte <klyte45@hotmail.com>
2024-04-14 19:53:32 -0300

    Fixing convention for loading resources

--- Utils/KResourceLoader.cs
+++ Utils/KResourceLoader.cs
@@ -8,7 +8,7 @@ namespace Belzont.Utils
     public static class KResourceLoader
     {
         public static Assembly RefAssemblyMod => BasicIMod.Instance.GetType().Assembly;
-        private static string NamespaceMod => $"{BasicIMod.Instance.SafeName}.";
+        private static string NamespaceMod => $"{BasicIMod.Instance.GetType().Namespace}.";
         public static Assembly RefAssemblyBelzont => typeof(KResourceLoader).Assembly;
 
         public static byte[] LoadResourceDataMod(string name) => LoadResourceData(NamespaceMod + name, RefAssemblyMod);
