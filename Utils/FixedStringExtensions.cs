a165b8951128172c2fd5e775050d9c2f76de2187
Leandro Klyte <klyte45@hotmail.com>
2024-04-14 22:03:18 -0300

    Utility to serialize fixed strings

---
+++ Utils/FixedStringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Colossal.Serialization.Entities;
+using Unity.Collections;
+
+namespace Belzont.Utils
+{
+    public static class FixedStringExtensions
+    {
+        public static void Write(this IWriter writer, FixedString512Bytes text)
+        {
+            var arraySave = new NativeArray<byte>(ZipUtils.Zip(text.ToString()), Allocator.Temp);
+            writer.Write(arraySave.Length);
+            writer.Write(arraySave);
+            arraySave.Dispose();
+        }
+
+        public static void Read(this IReader reader, out FixedString512Bytes text)
+        {
+            reader.Read(out int size);
+            NativeArray<byte> byteNativeArray = new(new byte[size], Allocator.Temp);
+            reader.Read(byteNativeArray);
+            try
+            {
+                text = ZipUtils.Unzip(byteNativeArray.ToArray());
+            }
+            catch
+            {
+                text = "<FAILED LOADING>";
+            }
+            byteNativeArray.Dispose();
+
+        }
+    }
+}
