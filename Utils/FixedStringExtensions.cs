5639fb444904db6dafbb46063efdd47e1325f466
Leandro Klyte <klyte45@hotmail.com>
2024-08-21 15:22:13 -0300

    Removing compression when serializing fixed strings

--- Utils/FixedStringExtensions.cs
+++ Utils/FixedStringExtensions.cs
@@ -7,17 +7,7 @@ namespace Belzont.Utils
     {
         public static void Write(this IWriter writer, FixedString512Bytes text)
         {
-            var arraySave = new NativeArray<byte>(ZipUtils.Zip(text.ToString()), Allocator.Temp);
-            writer.Write(arraySave.Length);
-            writer.Write(arraySave);
-            arraySave.Dispose();
-        }
-        public static void Write(this IWriter writer, string text)
-        {
-            var arraySave = new NativeArray<byte>(ZipUtils.Zip(text ?? ""), Allocator.Temp);
-            writer.Write(arraySave.Length);
-            writer.Write(arraySave);
-            arraySave.Dispose();
+            writer.Write(text.ToString());
         }
 
         public static void Read(this IReader reader, out FixedString512Bytes text) => text = ReadString(reader);
@@ -25,24 +15,11 @@ namespace Belzont.Utils
         public static void Read(this IReader reader, out FixedString64Bytes text) => text = ReadString(reader);
         public static void Read(this IReader reader, out FixedString32Bytes text) => text = ReadString(reader);
         public static void Read(this IReader reader, out FixedString4096Bytes text) => text = ReadString(reader);
-        public static void Read(this IReader reader, out string text) => text = ReadString(reader).TrimToNull();
 
         private static string ReadString(IReader reader)
         {
-            string text;
-            reader.Read(out int size);
-            NativeArray<byte> byteNativeArray = new(new byte[size], Allocator.Temp);
-            reader.Read(byteNativeArray);
-            try
-            {
-                text = ZipUtils.Unzip(byteNativeArray.ToArray());
-            }
-            catch
-            {
-                text = "<FAILED LOADING>";
-            }
-            byteNativeArray.Dispose();
-            return text;
+            reader.Read(out string value);
+            return value ?? "";
         }
     }
 }
