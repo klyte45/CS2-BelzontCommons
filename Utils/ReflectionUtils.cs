97382080367a92cca9204239c3eb3660db1ddc3a
Leandro Klyte <klyte45@hotmail.com>
2024-08-31 18:47:19 -0300

    Chunk byte array utility

--- Utils/ReflectionUtils.cs
+++ Utils/ReflectionUtils.cs
@@ -657,4 +657,18 @@ namespace Belzont.Utils
 
         #endregion
     }
+    public static class ByteArrayUtils
+    {
+        public static IEnumerable<byte[]> Chunk(this byte[] value, int bufferLength)
+        {
+            int countOfArray = value.Length / bufferLength;
+            if (value.Length % bufferLength > 0)
+                countOfArray++;
+            for (int i = 0; i < countOfArray; i++)
+            {
+                yield return value.Skip(i * bufferLength).Take(bufferLength).ToArray();
+
+            }
+        }
+    }
 }
