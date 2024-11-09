259fd5a00d99bdc51c4282ee1eaf57f20beef875
Leandro Klyte <klyte45@hotmail.com>
2024-11-09 10:24:20 -0300

    Adding KGuidUtils

---
+++ Utils/KGuidUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.Mathematics;
+
+namespace Belzont.Utils
+{
+    public static class KGuidUtils
+    {
+        public unsafe static Guid ToGuid(this Colossal.Hash128 hash128)
+        {
+            uint4* pnt = &hash128.value;
+            return *(Guid*)pnt;
+        }
+    }
+}
