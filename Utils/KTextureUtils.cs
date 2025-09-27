5bb866ec3a66b3bcd3cc66609898c93dabfd1854
Leandro Klyte <klyte45@hotmail.com>
2025-09-27 06:19:40 -0300

    Utility to read image that are not readable

--- Utils/KTextureUtils.cs
+++ Utils/KTextureUtils.cs
@@ -89,5 +89,25 @@ namespace Belzont.Utils
         {
             return (imageData[offset] << 8) | imageData[offset + 1];
         }
+        public static Texture2D MakeReadable(this Texture texture)
+        {
+            if (texture is Texture2D t2d && t2d.isReadable) return t2d;
+            RenderTexture temporary = RenderTexture.GetTemporary(texture.width, texture.height, 0);
+            Graphics.Blit(texture, temporary);
+            Texture2D result = temporary.ToTexture2D();
+            RenderTexture.ReleaseTemporary(temporary);
+            return result;
+        }
+
+        public static Texture2D ToTexture2D(this RenderTexture rt)
+        {
+            RenderTexture active = RenderTexture.active;
+            RenderTexture.active = rt;
+            Texture2D texture2D = new Texture2D(rt.width, rt.height);
+            texture2D.ReadPixels(new Rect(0f, 0f, rt.width, rt.height), 0, 0);
+            texture2D.Apply();
+            RenderTexture.active = active;
+            return texture2D;
+        }
     }
 }
